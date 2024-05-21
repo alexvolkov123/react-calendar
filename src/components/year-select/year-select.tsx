2ec548adb2ff53ffbc539372bd54244fc6f97042
LuckyLuxem <alexvolkov123098@gmail.com>
2024-05-21 18:04:50 +0300

    added custom year-select component

---
+++ src/components/year-select/year-select.tsx
@@ -0,0 +1,27 @@
+import { ArrowLeft, ArrowRight } from '@mui/icons-material'
+import { IconButton, Typography } from '@mui/material'
+import { add, sub } from 'date-fns'
+
+import { ISelect } from '../../types/types'
+import './year-select.css'
+
+export default function YearSelect({ today, setToday }: ISelect) {
+	function handleChangeYear(changeYear: boolean) {
+		!changeYear
+			? setToday(sub(today, { years: 1 }))
+			: setToday(add(today, { years: 1 }))
+	}
+	return (
+		<div className='year-select'>
+			<IconButton onClick={() => handleChangeYear(false)}>
+				<ArrowLeft className='year-select__item' />
+			</IconButton>
+			<Typography className='year-select__item'>
+				{today.getFullYear()}
+			</Typography>
+			<IconButton onClick={() => handleChangeYear(true)}>
+				<ArrowRight className='year-select__item' />
+			</IconButton>
+		</div>
+	)
+}
